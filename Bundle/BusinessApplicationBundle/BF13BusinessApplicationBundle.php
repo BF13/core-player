5d0590838c76b0f956146786e70e8d8336d12236
FYAM <fyamani@free.fr>
2013-04-12 16:55:11 +0300

    Create BF13BusinessApplicationBundle.php

---
+++ Bundle/BusinessApplicationBundle/BF13BusinessApplicationBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BF13\BusinessApplicationBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Bundle.
+ *
+ * @author FYAMANI
+ */
+class BF13businessApplicationBundle extends Bundle
+{
+}
