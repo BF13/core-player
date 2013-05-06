04a27446a21c140d132573a0814d4909639c908f
fyamani <fyamani@free.fr>
2013-05-06 17:15:08 +0200

    Update Querizer

--- src/BF13/Component/Storage/Tests/Doctrine/QuerizerTest.php
+++ src/BF13/Component/Storage/Tests/Doctrine/QuerizerTest.php
@@ -43,4 +43,48 @@ class QuerizerTest extends \PHPUnit_Framework_TestCase
 
         $this->assertEquals('bb', $items[1]);
     }
+
+    public function testRetrieveOne()
+    {
+        $value = array(
+                'from' => 'Entity', 
+                'columns' => array('id' => array('field' => 'e.id'), 'name' => array('field' => 'e.name')), 
+                'conditions' => array(
+                    'test_condition' => array('items' => array('id' => 'e.id = :id'))
+                ), 
+                'joins' => array()
+        );
+        
+        $this->stub_repository->expects($this->any())
+            ->method('getDefaultSchema')
+            ->will($this->returnValue($value))
+        ;
+
+        $data = array(array('id' => '1', 'name' => 'My Entity'));
+        
+        $this->stub_builder->expects($this->any())
+            ->method('getQuery')
+            ->will($this->returnSelf())
+        ;
+
+        $this->stub_builder->expects($this->any())
+            ->method('getResult')
+            ->will($this->returnValue($data))
+        ;
+
+        $this->querizer = new Querizer($this->stub_repository, $this->stub_builder);
+        
+        $fields = array('id');
+        
+        $this->querizer
+        ->datafields($fields)
+        ->group(array('name'))
+        ->sort(array('name' => 'DESC'))
+        ->conditions(array('test_condition' => 1))
+        ;
+        
+        $item = $this->querizer->result();
+        
+        $this->assertEquals('My Entity', $item['name']);
+    }
 }
