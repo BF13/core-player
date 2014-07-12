e01807c627756ac0144146fea155c34a7fb7d7d7
fouad.yamani <fyamani@acefy.fr>
2014-07-12 09:43:37 +0000

    fix test

--- src/BF13/Component/ValueList/Tests/ValueListTest.php
+++ src/BF13/Component/ValueList/Tests/ValueListTest.php
@@ -8,60 +8,60 @@ class ValueListTest extends \PHPUnit_Framework_TestCase
     public function setUp()
     {
         $values = array(
-                array('id' => 1, 'value_key' => 'ouinon.oui', 'value' => 'oui', 'list_key' => 'ouinon'),
-                array('id' => 2, 'value_key' => 'ouinon.non', 'value' => 'non', 'list_key' => 'ouinon'),
+                array('id' => 1, 'vlkey' => 'ouinon.oui', 'data' => array('fr' => 'oui')),
+                array('id' => 2, 'vlkey' => 'ouinon.non', 'data' => array('fr' => 'non')),
         );
-        
+
         $repository = $this->getMockBuilder('BF13\Component\Storage\StorageConnectorInterface')
         ->disableOriginalConstructor()
         ->setMethods(array('getQuerizer', 'datafields', 'results', 'getHandler'))
         ->getMock();
         ;
-        
+
         $repository->expects($this->any())
         ->method('getQuerizer')
         ->will($this->returnSelf())
         ;
-        
+
         $repository->expects($this->any())
         ->method('datafields')
         ->will($this->returnSelf())
         ;
-        
+
         $repository->expects($this->any())
         ->method('results')
         ->will($this->returnValue($values))
         ;
-        
+
         $this->valueList = new ValueList($repository);
     }
-    
+
     public function testSuccessShowList()
     {
         $list = $this->valueList->getListValues('ouinon');
-        
+
         $this->assertEquals(2, sizeOf($list));
-        
+
         $this->assertEquals('oui', $list['ouinon.oui']);
-        
+
         $this->assertEquals('non', $list['ouinon.non']);
     }
-    
+
     public function testNotExistingList()
     {
         $list = $this->valueList->getListValues('ouinonx');
-        
+
         $this->assertTrue(is_array($list));
-        
+
         $this->assertEquals(0, sizeOf($list));
     }
-    
+
     public function testNullList()
     {
         $list = $this->valueList->getListValues();
-        
+
         $this->assertTrue(is_array($list));
-        
+
         $this->assertEquals(0, sizeOf($list));
     }
 }
